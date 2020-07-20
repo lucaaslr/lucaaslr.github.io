d36899d8cd29033c80d4cf30888dd0c63874d081
lucaaslr <lucasramalho@gmail.com>
2020-07-20 08:31:24 -0300

    Adding blog page

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    
+    $('.post_wrapper').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+    });
+
+});
+
+
