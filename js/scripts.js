5ec25adfb6ce0de62c63cc4ccb83446bd7e40729
lucaaslr <lucasramalhofma@gmail.com>
2020-08-08 20:42:38 -0700

    - Update  blog page - Delete about_me - reorg code structure

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,34 @@ $(document).ready(function() {
         slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 1000,
+        autoplaySpeed: 2000,
         nextArrow: $('.next'),
         prevArrow: $('.prev'),
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: true,
+                nextArrow: $('.next'),
+                prevArrow: $('.prev')
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                nextArrow: $('.next'),
+                prevArrow: $('.prev')
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
     });
 
 });
